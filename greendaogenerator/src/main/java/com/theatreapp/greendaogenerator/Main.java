3775d71762a4484f4ba5fcf36f03dede25b09ae1
nathalieborodina <nata.britan97@gmail.com>
2017-04-10 13:00:32 +0300

    database entities added

--- greendaogenerator/src/main/java/com/theatreapp/greendaogenerator/Main.java
+++ greendaogenerator/src/main/java/com/theatreapp/greendaogenerator/Main.java
@@ -7,7 +7,7 @@ import de.greenrobot.daogenerator.Schema;
 public class Main {
 
     public static final int DB_VERSION = 1000;
-    public static final String OUTPUT_PACKAGE = "com.nathalieborodina.theatreapp.data.model";
+    public static final String OUTPUT_PACKAGE = "com.nathalieborodina.theatreapp";
     public static final String OUTPUT_FOLDER = "app/src/main/java";
 
     public static void main(String[] args) throws Exception {
@@ -15,11 +15,25 @@ public class Main {
         Schema schema = new Schema(DB_VERSION, OUTPUT_PACKAGE);
 
         new DaoGenerator().generateAll(schema, OUTPUT_FOLDER);
+
+        addPerfomance(schema);
+        addPerfomanceEvent(schema);
+
+        new DaoGenerator().generateAll(schema, OUTPUT_FOLDER);
+    }
+
+    private static void addPerfomanceEvent(Schema schema) {
+        Entity perfomanceEvent = schema.addEntity("PerfomanceEvent");
+        perfomanceEvent.addIdProperty().autoincrement();
+        perfomanceEvent.addLongProperty("eventId");
+        perfomanceEvent.addDateProperty("date_time");
     }
 
     private static void addPerfomance(Schema schema) {
-
-
+        Entity perfomance = schema.addEntity("Perfomance");
+        perfomance.addIdProperty().autoincrement();
+        perfomance.addLongProperty("eventId");
+        perfomance.addStringProperty("title");
     }
 
 }
